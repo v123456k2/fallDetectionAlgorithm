08ff21c25fc9f6b5d5385b729aaa0f6e491129ec
Vincent Chow <v123456k212561256@gmail.com>
2021-12-09 23:36:26 -0500

    Add files via upload

---
+++ FallDetectionDevice/firmware/FallDetection.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32cm_mc00_cnano configuration
+CND_ARTIFACT_DIR_pic32cm_mc00_cnano=dist/pic32cm_mc00_cnano/production
+CND_ARTIFACT_NAME_pic32cm_mc00_cnano=FallDetection.X.production.hex
+CND_ARTIFACT_PATH_pic32cm_mc00_cnano=dist/pic32cm_mc00_cnano/production/FallDetection.X.production.hex
+CND_PACKAGE_DIR_pic32cm_mc00_cnano=${CND_DISTDIR}/pic32cm_mc00_cnano/package
+CND_PACKAGE_NAME_pic32cm_mc00_cnano=falldetection.x.tar
+CND_PACKAGE_PATH_pic32cm_mc00_cnano=${CND_DISTDIR}/pic32cm_mc00_cnano/package/falldetection.x.tar
